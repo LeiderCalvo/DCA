04bdf1d25edec2bc0ec9c9deeba9a389cea928ff
Juan Esteban Pérez <43039970+juanez1999@users.noreply.github.com>
2024-10-08 12:56:42 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,42 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, doc, getDocs, collection, setDoc } from 'firebase/firestore/lite';
+import { getFirestore } from 'firebase/firestore';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyD-HYIh8x0onCBChk1wlSDlWIeSUdaFXNM',
-	authDomain: 'dcatest-2ee5f.firebaseapp.com',
-	projectId: 'dcatest-2ee5f',
-	storageBucket: 'dcatest-2ee5f.appspot.com',
-	messagingSenderId: '634779052994',
-	appId: '1:634779052994:web:e23d3ec2eb58c9d2f6045b',
-	measurementId: 'G-V8GF33RZ1M',
+	apiKey: 'AIzaSyDdeXwNTkI8lHfmjLFhtC4FbJvfGKIr9_s',
+	authDomain: 'apps-reto1.firebaseapp.com',
+	databaseURL: 'https://apps-reto1.firebaseio.com',
+	projectId: 'apps-reto1',
+	storageBucket: 'apps-reto1.appspot.com',
+	messagingSenderId: '470696037037',
+	appId: '1:470696037037:web:bb70b5a3b1d74e11ddd424',
+	measurementId: 'G-DRDDR7JWBM',
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const citiesRef = collection(db, 'test');
-setDoc(doc(citiesRef, 'SF'), {
-	name: 'San Francisco',
-	state: 'CA',
-	country: 'USA',
-	capital: false,
-	population: 860000,
-	regions: ['west_coast', 'norcal'],
-});
+const addUser = async () => {
+	try {
+		const docRef = await addDoc(collection(db, 'test'), {
+			first: 'Ada',
+			last: 'Lovelace',
+			born: 1815,
+		});
+		console.log('Document written with ID: ', docRef.id);
+	} catch (e) {
+		console.error('Error adding document: ', e);
+	}
+};
 
-const docRef = collection(db, 'test');
-const querySnapshot = await getDocs(docRef);
+// addUser();
 
-querySnapshot.forEach((doc) => {
-	// doc.data() is never undefined for query doc snapshots
-	console.log(doc.id, ' => ', doc.data());
-});
+const getUsers = async () => {
+	const querySnapshot = await getDocs(collection(db, 'posts'));
+	querySnapshot.forEach((doc) => {
+		console.log(doc.data());
+	});
+};
+
+getUsers();
