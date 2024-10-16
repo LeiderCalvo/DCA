63100bc412525f0eddc99b4347ab9c2c70b49af0
Juan Esteban Pérez <43039970+juanez1999@users.noreply.github.com>
2024-10-15 21:12:08 -0500

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,40 +1,55 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+let db: any;
 
-const firebaseConfig = {
-	apiKey: 'AIzaSyDdeXwNTkI8lHfmjLFhtC4FbJvfGKIr9_s',
-	authDomain: 'apps-reto1.firebaseapp.com',
-	databaseURL: 'https://apps-reto1.firebaseio.com',
-	projectId: 'apps-reto1',
-	storageBucket: 'apps-reto1.appspot.com',
-	messagingSenderId: '470696037037',
-	appId: '1:470696037037:web:bb70b5a3b1d74e11ddd424',
-	measurementId: 'G-DRDDR7JWBM',
+const getFirestoreInstance = async () => {
+	if (!db) {
+		const { getFirestore } = await import('firebase/firestore');
+		const { initializeApp } = await import('firebase/app');
+
+		const firebaseConfig = {
+			apiKey: 'AIzaSyDdeXwNTkI8lHfmjLFhtC4FbJvfGKIr9_s',
+			authDomain: 'apps-reto1.firebaseapp.com',
+			databaseURL: 'https://apps-reto1.firebaseio.com',
+			projectId: 'apps-reto1',
+			storageBucket: 'apps-reto1.appspot.com',
+			messagingSenderId: '470696037037',
+			appId: '1:470696037037:web:6de930ceb2df9d14ddd424',
+			measurementId: 'G-G9ELH58HS7',
+		};
+
+		const app = initializeApp(firebaseConfig);
+		db = getFirestore(app);
+	}
+	return db;
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 export const addProduct = async (product: any) => {
 	try {
+		const db = await getFirestoreInstance();
+		const { collection, addDoc } = await import('firebase/firestore');
+
 		const where = collection(db, 'products');
 		await addDoc(where, product);
-		console.log('Se añadió con éxito');
-	} catch (e) {
-		console.error('Error adding document: ', e);
+		console.log('Se añadió con exito');
+	} catch (error) {
+		console.error('Error adding document', error);
 	}
 };
 
 export const getProducts = async () => {
-	const where = collection(db, 'products');
-	const querySnapshot = await getDocs(where);
-	const data: any[] = [];
+	try {
+		const db = await getFirestoreInstance();
+		const { collection, getDocs } = await import('firebase/firestore');
 
-	querySnapshot.forEach((doc) => {
-		data.push(doc);
-	});
+		const where = collection(db, 'products');
+		const querySnapshot = await getDocs(where);
+		const data: any[] = [];
 
-	return data;
+		querySnapshot.forEach((doc) => {
+			data.push(doc.data());
+		});
+
+		return data;
+	} catch (error) {
+		console.error('Error getting documents', error);
+	}
 };
